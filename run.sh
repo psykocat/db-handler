0ae43741249716c69389b13a273db65456fdd97e
PsykoCat <PsykoCat at nomail dot localhost>
2021-06-01 17:58:39 +0200

    Generate default passwords

--- run.sh
+++ run.sh
@@ -19,11 +19,17 @@ if [ "${build_image}" = "yes" ]; then
 	docker-compose build -q
 fi
 
+. ./.env
+
 # Create empty passfiles if absent for the image run
-for passfile in ./db_password ./db_root_password; do
-	[ -s "${passfile}" ] || echo "empty" > "${passfile}"
+for passfile in ${LOCAL_DB_ROOT_PASSWORD_FILE} ${LOCAL_DB_PASSWORD_FILE}; do
+	[ -s "${passfile}" ] || openssl rand -base64 32 > "${passfile}"
 done
 unset -- passfile
+
+if [ ! -d "${SHARED_VOLUME_DIR}" ]; then
+	mkdir -p "${SHARED_VOLUME_DIR}"
+fi
 
 docker-compose run --rm dbhandler "${args[@]}"
 
