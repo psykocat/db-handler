1341acce8ed6d20ac0312082d3d779153bef317d
PsykoCat <PsykoCat at nomail dot localhost>
2021-06-21 19:42:16 +0200

    Use automated connection when no argument is provided

--- scripts/psql-connect.sh
+++ scripts/psql-connect.sh
@@ -9,6 +9,8 @@ if [ -n "${DB_ROOT_USER:-}" ]; then
 fi
 file_env DB_NAME
 
+__conn_user="DB_USER"
+
 # subscript help
 _sub_usage(){
 	cat >> /dev/stderr <<-EOF
@@ -20,16 +22,47 @@ _sub_usage(){
 
 # Input the command to execute in a _main() function
 _main(){
+	local _args=()
+
+	while [ ${#} -gt 0 ]; do
+		case ${1} in
+			--debug)
+				set -x
+				;;
+			--)
+				shift;
+				for _remain in ${@}; do
+					_args+=("${_remain}")
+				done
+				unset -- _remain
+				shift $#
+				break
+				;;
+			-help|--help)
+				_sub_usage
+				exit
+				;;
+			--root)
+				if [ -z "${DB_ROOT_USER:-}" ]; then
+					log_err "No user infos provided. Please fill DB_ROOT_USER and DB_ROOT_PASSWORD variables."
+					exit 1
+				fi
+				__conn_user="DB_ROOT_USER"
+				;;
+			*)
+				_args+=("${1}")
+				;;
+		esac
+		shift
+	done
+	if [ ${#_args[*]} -eq 0 ]; then
+		set -- -h "${DB_HOST}" -U "${!__conn_user}" "${DB_NAME}"
+	fi
 	_setup_pgpass
 	log_inf psql "${@}"
 	#psql -h ${DB_HOST} -p ${DB_PORT} -U ${DB_USER} "${@}"
 	psql "${@}"
 }
-
-if echo $*|grep -qwe "-help\|--help"; then
-	_sub_usage
-	exit
-fi
 
 _main "${@}"
 
